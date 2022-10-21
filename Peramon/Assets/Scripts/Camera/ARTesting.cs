cc3bb42b5c6f5d95984adb4e52dec598f7262600
らく <katz03010@gmail.com>
2022-10-21 14:48:35 +0900

    モンスターの素材、当たり判定追加。 画像認識追加、カメラ更新。

--- Peramon/Assets/Scripts/Camera/ARTesting.cs
+++ Peramon/Assets/Scripts/Camera/ARTesting.cs
@@ -1,33 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
 public class ARTesting : MonoBehaviour
 {
-    [SerializeField] GameObject redCube;//生成するオブジェクト
-    private ARRaycastManager arRaycastManager;
-    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
-     
- 
+    [SerializeField] Text text, score;
+    [SerializeField] Camera arCamera;
+    int m_score = 0;
+
+
+
     void Start()
     {
-        arRaycastManager = GetComponent<ARRaycastManager>();
     }
-    
+
     void Update()
     {
-        if(Input.touchCount > 0)
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            if(touch.phase == TouchPhase.Began)//画面に指が触れた時に処理する
+            if (touch.phase == TouchPhase.Began)
             {
-                if (arRaycastManager.Raycast(touch.position, hits, TrackableType.PlaneWithinPolygon))
+                Ray ray = arCamera.ScreenPointToRay(touch.position);
+                RaycastHit hitObject;
+                if (Physics.Raycast(ray, out hitObject))
                 {
-                    Pose hitPose = hits[0].pose;//RayとARPlaneが衝突しところのPose
-                    Instantiate(redCube, hitPose.position, hitPose.rotation);//オブジェクトの生成
+                    text.text = hitObject.transform.gameObject.name;
+                    Destroy(hitObject.transform.gameObject);
+                    m_score++;
+                    score.text = m_score + "/5";
                 }
             }
         }
