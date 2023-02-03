f59772d63ab437af5fe20079472ce567860eb1a1
Violetkuroinu <21c1020055wt@edu.tech.ac.jp>
2023-02-03 16:02:13 +0900

    UI変更。リザルト画面

--- Peramon/Assets/Scripts/Monster/MonsterData.cs
+++ Peramon/Assets/Scripts/Monster/MonsterData.cs
@@ -2,10 +2,10 @@ public static class MonsterData
 {
     public enum MonsterID
     {
-        Mon = 0,
-        Foo,
-        Garake,
-        Tubotubo,
-        Denkyu,
+        Mon = 0, //0
+        Foo,     //1
+        Garake,  //2
+        Tubotubo,//3
+        Denkyu,  //4
     }
 }
