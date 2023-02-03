4462f75b3c1a4b9ea81aa12b831d9c5edbb11808
らく <katz03010@gmail.com>
2023-02-03 18:20:21 +0900

    1枚の画像でモンスター４体が出てくるように調整

--- Peramon/Assets/Scripts/ImageTracker/MonsterSpawner.cs
+++ Peramon/Assets/Scripts/ImageTracker/MonsterSpawner.cs
@@ -11,15 +11,45 @@ public class MonsterSpawner : MonoBehaviour
 
     public void SpawningObject(int imageNum, Vector3 pos, Quaternion rot)
     {
-        GameObject monster;
-        monster = Instantiate(_arObjManager.GetArObjByNum(RandomMonsterNum(imageNum)), pos, rot);
-        BaseMonster _baseMonster = monster.transform.GetChild(0).gameObject.GetComponent<BaseMonster>();
-        _baseMonster.isSpecial = RandomMonsterSpecial(30);
-        _baseMonster.isSpecial = true ? 
-                                        _baseMonster.haveTreasure = RandomMonsterSpecial(80) : 
-                                        _baseMonster.haveTreasure = RandomMonsterSpecial(50) ;
+        //int[] _monsterArray =  RandomMonsterNumArray(imageNum);
+        for(int i = 0; i < 4; i++)
+        {
+            int _monsterID = 0;
+            if(imageNum == 0) _monsterID = i;
+            else if(imageNum == 1)_monsterID = i + 4;
+            GameObject monster = Instantiate(_arObjManager.GetArObjByNum(_monsterID), MonsterPos(i,pos), rot);
+            BaseMonster _baseMonster = monster.transform.GetChild(0).gameObject.GetComponent<BaseMonster>();
+            _baseMonster.isSpecial = RandomMonsterSpecial(30);
+            _baseMonster.isSpecial = true ?
+                                            _baseMonster.haveTreasure = RandomMonsterSpecial(80) :
+                                            _baseMonster.haveTreasure = RandomMonsterSpecial(50);
+        }
         Debug.Log("Spawn");
     }
+
+    private Vector3 MonsterPos(int _monsterNum, Vector3 _oPos)
+    {
+        Vector3 _pos = _oPos;
+        switch(_monsterNum)
+        {
+            case 0:
+                _pos = _oPos + new Vector3(0.1f,0f,0.1f);
+                break;
+            case 1:
+                _pos = _oPos + new Vector3(-0.1f,0f,0.1f);
+                break;
+            case 2:
+                _pos = _oPos + new Vector3(-0.1f,0f,-0.1f);
+                break;
+            case 3:
+                _pos = _oPos + new Vector3(0.1f,0f,-0.1f);
+                break;
+            default:
+                break;
+        }
+        return _pos;
+    }
+
 
     private int RandomMonsterNum(int _num)
     {
