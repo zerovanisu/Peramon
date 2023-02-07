388ea9ff791846a3f962d1127a9fb2fd85ee623c
らく <katz03010@gmail.com>
2023-02-08 02:21:57 +0900

    新しいモンスター追加、 マージ衝突修正

--- Peramon/Assets/Ryu-6/SoundEvent.cs
+++
@@ -1,7 +0,0 @@
-public enum Events
-{
-    None, // 0
-    PlayBGM = 200,
-    PlayJNG = 201,
-    PlaySE = 202,
-}
--- Peramon/Assets/Scripts/Monster/BaseMonster.cs
+++ Peramon/Assets/Scripts/Monster/BaseMonster.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class BaseMonster : MonoBehaviour
 {
     public bool isSpecial;
-    private bool haveTreasure;
+    public bool haveTreasure;
+    public int touchCount = 0;
     private bool isKilled;
     private Vector3 spawnPosition;
     [SerializeField]public int monsterID;
     // Start is called before the first frame update
     void Start()
     {
-
+        touchCount = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(touchCount>=3 && isSpecial)
+            isSpecial = false;
     }
 }
